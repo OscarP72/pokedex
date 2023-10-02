273a77aee80e0d67c6023bc259d1a52722eed88a
UsuarioM <oskitarpereal@gmail.com>
2023-10-02 14:24:22 +0200

    a falta de maquetar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    number: "",
+    species: "",
+    image: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    speed: "",
+    type: "",
+  });
+  const searchPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((res) => {
+        setPokemon({
+          name: pokemonName,
+          number: res.data.id,
+          species: res.data.species.name,
+          image: res.data.sprites.front_default,
+          hp: res.data.stats[0].base_start,
+          attack: res.data.stats[1].base_stat,
+          defense: res.data.stats[2].base_stat,
+          speed: res.data.stats[5].base_stat,
+          type: res.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
+      });
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="TitleSection">
+        <h1>Pokédex</h1>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPokemonName(event.target.value);
+          }}
+          value={pokemonName.toLocaleLowerCase()}
+        />
+        <div>
+        {pokemonName && <button onClick={searchPokemon}>Busqueda de Pokémon</button>}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="DisplaySection">
+        {!pokemonChosen ? (
+          <h1> Please choose a Pokémon </h1>
+        ) : (
+          <>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.image} alt={pokemon.name} />
+            <h3>Number: #{pokemon.number}</h3>
+            <h3>Species: {pokemon.species}</h3>
+            <h3>Type: {pokemon.type}</h3>
+            <h4>Hp: {pokemon.hp}</h4>
+            <h4>Attack: {pokemon.attack}</h4>
+            <h4>Defense: {pokemon.defense}</h4>
+            <h4>Speed: {pokemon.speed}</h4>
+          </>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
